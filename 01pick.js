76f68347b9ae84e8128c629893798755c994094c
ANZOR007 <AnzorZhukov@gmail.com>
2018-12-20 18:05:13 +0300

    Instal commit

---
+++ 01pick.js
@@ -0,0 +1,20 @@
+
+function pick(mass)
+{
+   return mass[Math.floor(Math.random() * mass.length)];
+}
+
+var list = [ 'qq', 'ww'];/*, 'ee','rr','tt','yy','uu','ii']; */
+console.log(" ");
+console.log("list           = ",list);
+console.log("list.length    = ",list.length);
+console.log("pick(list)     = ",pick(list));
+console.log(" ");
+
+
+/* ---------------------------------------------------------------------------- /**/
+/* Задача 1                                                                     /**/
+/* Написать функцию pick(list), где list - массив,                              /**/
+/* которая возвращает произвольный элемент из list.                             /**/
+/* Подсказки: Math.random, list.length                                          /**/
+/* ---------------------------------------------------------------------------- /**/
