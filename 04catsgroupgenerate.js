1e6c6ce2a6ec91fa8228f2fdfe91f1acc7278c78
Anzor007 <AnzorZhukov@gmail.com>
2018-12-24 18:40:23 +0300

    666 777 13 07

--- 04catsgroupgenerate.js
+++ 04catsgroupgenerate.js
@@ -4,7 +4,7 @@ function catFactory()
     {
         return mass[Math.floor(Math.random() * mass.length)];
     }
- 
+
     var CatFac = {}; /*создание котообъекта*/
     var mass_name = ['Айсберг','Аскольд','Астерикс' ,'Аякс'     ,'Артур'    ,'Асклепий','Арес'    ,'Атос'    ,'Арамис'  ,'Август'  ,'Альбус'  ,'Алмаз'  ,
                      'Васаби' ,'Винсент','Валдис'   ,'Вирус'    ,'Ватсон'   ,'Висмут'  ,'Вермут'  ,'Витас'   ,'Витязь'  ,'Влас'    ,'Василек' ,'Вольтер',
@@ -22,7 +22,7 @@ function catFactory()
     var mass_gender = ['Мужской','Женский','Непонятный'];
     var mass_legsCount = ['4','3'];
     var mass_tailLength  = ['1','3','5','7','9','11','13','15','25'];
-    
+
     CatFac.name = pick(mass_name);
     CatFac.age = pick(mass_age);
     CatFac.gender = pick(mass_gender);
@@ -63,7 +63,7 @@ const list = catsGroupGenerate(n);
 function catMen(catsGGList)
 {/* - котов мужского пола /**/
     var arraCatMen = [];
-    
+
     var counter = 0;
 
     for (var i=0; i<n; i++)
@@ -73,7 +73,7 @@ function catMen(catsGGList)
             arraCatMen[counter] = catsGGList[i];
             //console.log (" Super Man =",arraCatMen[counter]);
             counter++;
-        } 
+        }
      }
      if (counter===0)
      {
@@ -96,9 +96,9 @@ function catName(catsGGList)
     for (var i=0; i<n; i++)
     {
         arraCatName[i] = catsGGList[i].name;
-        //console.log (" Cat Name =",arraCatName[i]);   
+        //console.log (" Cat Name =",arraCatName[i]);
      }
-     console.log (" Cat Name All=",arraCatName);   
+     console.log (" Cat Name All=",arraCatName);
      return arraCatName;
 }
 console.log (" ");
@@ -108,11 +108,11 @@ console.log ("-------------------------------------------------- catName END ---
 
 function catOld(catsGGList,n0)
 {/* - n самых старых котов (функция также принимает параметр n - максимальный размер результата)   /**/
-    var t_arracatAge=[]; 
-    //var tt_catAge=0; 
-    //var tt_catAge_i=0;
-    
-    
+    var t_arracatAge=[];
+    var tt_catAge=0;
+    var tt_catAge_i=0;
+
+
     for (var i0=0; i0<n0; i0++)
     {
         t_arracatAge[i0]=catsGGList[i0].age;
@@ -120,17 +120,20 @@ function catOld(catsGGList,n0)
     console.log(" \nfufel\n",t_arracatAge);
 
 
-    
+
 
     for (var i=0; i<catsGGList.length-1; i++)
-    { 
+    {
         for (var j=i+1; j<=catsGGList.length; j++)
-        if (t_catAge[i].age > t_catAge[j].age)
         {
-
-            tt_catAge=t_catAge[i]; tt_catAge_i=[i];
-            t_catAge[i]=t_catAge[j];
-            t_catAge[j]=tt_catAge[tt_catAge_i];
+            console.log(" i=",i," j=",j)
+            //if (t_arracatAge[i].age >= t_arracatAge[j].age)
+            {
+              //  console.log(" ogo   ");
+                //tt_catAge=t_catAge[i]; tt_catAge_i=[i];
+  //              t_catAge[i]=t_catAge[j];
+    //            t_catAge[j]=tt_catAge[tt_catAge_i];
+            }
         }
     }
     console.log(" ",t_catAge_i);
